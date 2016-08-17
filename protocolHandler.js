55eedde113c2f8dfc672e6f99b637a138023df8e
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-08-18 01:35:55 +0300

    Implementing prototype that will save req/res

--- protocolHandler.js
+++ protocolHandler.js
@@ -1,35 +1,13 @@
-const net = require('net');
-const log = require('./lib/logger').get();
-const httpHandler = require('./lib/protocols/http').handle;
+const httpProtocol = require('./lib/protocols/http');
 
-const handlers = {
-  http: httpHandler,
+const protocols = {
+  http: httpProtocol,
 };
 
-const server = net.createServer((socket) => {
-  log.info('Client connected', socket.address());
-  handlers.http(socket);
-  socket.end('Good bye');
-});
-
 function start(port, cb) {
-  log.info(`Starting on port: ${port}`);
-  const opts = {
-    port,
-  };
-  server.listen(opts, () => {
-    const address = server.address();
-    log.info('Server started:', address);
-    cb();
-  });
-}
-
-function stop(cb) {
-  log.info('Stopping server');
-  server.close(cb);
+  protocols.http.listen(port, cb);
 }
 
 module.exports = {
   start,
-  stop,
 };
