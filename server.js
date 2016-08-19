bfaa45930b891fe8f7aede731ad8a505fa0385f2
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-08-19 14:48:13 +0300

    Some cleanup.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const config = require('config');
-const protocolHandler = require('./protocolHandler');
+const fs = require('fs');
+const proxy = require('./lib/proxy');
 
-protocolHandler.start(config.port, (error) => {
+const opts = {
+  port: config.port,
+  ssl: {
+    key: fs.readFileSync(config.get('ssl.key')),
+    cert: fs.readFileSync(config.get('ssl.cert')),
+  },
+};
+proxy.listen(opts, (error) => {
   if (error) throw error;
 });
