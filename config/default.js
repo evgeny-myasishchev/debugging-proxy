6097bfa8b91ba55d1de8bd7d6ab5095ecbbb00d3
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-08-21 00:43:27 +0300

    Some logging related config.

--- config/default.js
+++ config/default.js
@@ -2,6 +2,8 @@ module.exports = {
   port: process.env.PORT || 8080,
   log: {
     name: 'DEBUGGING-PROXY',
+    stdout: true,
+    file: null,
   },
   storage: {
     path: 'var/data',
--- config/test.js
+++ config/test.js
@@ -1,4 +1,8 @@
 module.exports = {
+  log: {
+    stdout: false,
+    file: 'test.log',
+  },
   storage: {
     memory: true,
   },
