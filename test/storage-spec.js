0dcce4deb32de6919a0fb0da16694b36abe3e61f
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-08-20 09:49:42 +0300

    Adding pending specs.

---
+++ test/storage-spec.js
@@ -0,0 +1,13 @@
+describe('storage', () => {
+  xdescribe('saveRequest', () => {
+    it('should save request with headers and body', () => {
+
+    });
+  });
+
+  xdescribe('saveResponse', () => {
+    it('should save response with headers and body', () => {
+
+    });
+  });
+});
