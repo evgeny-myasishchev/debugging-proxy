2871cef7560b94a826ef6f6cc71a05d1942670b0
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-09-05 09:53:31 +0300

    Dispatching selectRequest action.

--- app/ui/components/RequestListItem.jsx
+++ app/ui/components/RequestListItem.jsx
@@ -6,11 +6,12 @@ export default class RequestListItem extends Component {
 
   render() {
     const req = this.props.request;
+    const { selectRequest } = this.props.actions;
     return (
       <tr>
         <td><span className='tag tag-default'>{req.request.method}</span> </td>
         <td>
-          <button className="btn btn-link">
+          <button className="btn btn-link" onClick={() => selectRequest(req)}>
             {req.request.protocol}://{req.request.host}{req.request.path}
           </button>
         </td>
@@ -20,5 +21,6 @@ export default class RequestListItem extends Component {
 }
 
 RequestListItem.propTypes = {
-  request: PropTypes.object.isRequired
+  request: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired,
 }
--- test/ui/components/RequestListItem-spec.js
+++ test/ui/components/RequestListItem-spec.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
-import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import React from 'react';
+import sinon from 'sinon';
 import RequestListItem from '../../../app/ui/components/RequestListItem.jsx';
 import chance from '../../support/chance';
 
@@ -10,8 +11,7 @@ describe('components RequestListItem', () => {
     const props = _.merge({
       request: chance.data.savedRequest(),
       actions: {
-        dummyAction1: chance.word(),
-        dummyAction2: chance.word(),
+        selectRequest: sinon.spy(),
       },
     }, p);
 
@@ -26,7 +26,17 @@ describe('components RequestListItem', () => {
   it('should render RequestListItem for each request', () => {
     const { enzymeWrapper, props } = setup();
     const request = props.request.request;
+    const span = enzymeWrapper.find('span.tag.tag-default');
+    expect(span.text()).to.eql(request.method);
+
     const button = enzymeWrapper.find('button');
     expect(button.text()).to.eql(`${request.protocol}://${request.host}${request.path}`);
+  });
+
+  it('should select request on button click', () => {
+    const { enzymeWrapper, props } = setup();
+    const button = enzymeWrapper.find('button');
+    button.props().onClick();
+    expect(props.actions.selectRequest).to.have.been.calledWith(props.request);
   });
 });
