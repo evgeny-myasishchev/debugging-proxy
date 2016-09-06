923b961cf85c61fa851dce48425fafd6e7ff11d9
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-09-06 23:41:58 +0300

    Minor tweaks.

--- app/ui/components/RequestListItem.jsx
+++ app/ui/components/RequestListItem.jsx
@@ -7,12 +7,13 @@ export default class RequestListItem extends Component {
   render() {
     const { request, isSelected } = this.props;
     const { selectRequest } = this.props.actions;
+    const href = `${request.request.protocol}://${request.request.host}${request.request.path}`
     return (
       <tr className={isSelected ? 'table-active' : null}>
         <td><span className='tag tag-default'>{request.request.method}</span> </td>
         <td>
-          <button className="btn btn-link" onClick={() => selectRequest(request)}>
-            {request.request.protocol}://{request.request.host}{request.request.path}
+          <button title={href} className="btn btn-sm btn-link" onClick={() => selectRequest(request)}>
+            {href}
           </button>
         </td>
       </tr>
