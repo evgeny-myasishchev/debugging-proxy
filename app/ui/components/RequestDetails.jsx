81133802f7aec336952dee8bca6b2d5aa04255d6
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-09-06 23:51:59 +0300

    Adding some stuff.

--- app/ui/components/RequestDetails.jsx
+++ app/ui/components/RequestDetails.jsx
@@ -4,8 +4,19 @@ export default class RequestDetails extends Component {
   componentWillMount() {}
 
   render() {
+    const { 
+      request : { request } 
+    } = this.props;
+    const href = `${request.protocol}://${request.host}/${request.path}`
     return (
-      <div className='alert alert-info'>TODO: Request details will be here</div>
+      <div className='card card-outline-primary'>
+        <div className="card-block">
+          <div className="input-group">
+            <span className="input-group-addon" id="basic-addon1">{request.method}</span>
+            <input readOnly="true" type="text" className="form-control" aria-describedby="basic-addon1" value={href} />
+          </div>
+        </div>
+      </div>
     )
   }
 }
