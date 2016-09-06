255f046409b0eeb58e676b46ebd1066ebcbace4c
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-09-06 08:56:53 +0300

    Configuring remote devtools.

--- app/ui/store/configureStore.js
+++ app/ui/store/configureStore.js
@@ -1,28 +1,37 @@
 import { createStore, applyMiddleware, compose } from 'redux'
+import { persistState } from 'redux-devtools';
+import devTools from 'remote-redux-devtools';
 import thunk from 'redux-thunk'
-import createLogger from 'redux-logger'
 import api from '../middleware/api'
 import rootReducer from '../reducers'
-import DevTools from '../containers/DevTools.jsx'
 
 export default function configureStore(apiRoot, preloadedState) {
-  const store = createStore(
-    rootReducer,
-    preloadedState,
-    compose(
-      applyMiddleware(thunk, api(apiRoot), createLogger()),
-      DevTools.instrument()
-    )
-  )
+  const enhancer = compose(
+    applyMiddleware(thunk, api(apiRoot)), 
+    devTools({
+      realtime : process.env.NODE_ENV !== 'production',
+      hostname : 'localhost',
+      port : 8000
+    }),
+    persistState(getDebugSessionKey)
+  );
+  const store = createStore(rootReducer, preloadedState, enhancer);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
       const nextRootReducer = require('../reducers').default
-
+  
       store.replaceReducer(nextRootReducer)
     })
   }
 
+  devTools.updateStore(store);
+
   return store
+}
+
+function getDebugSessionKey() {
+  const matches = window.location.href.match(/[?&]debug-session=([^&#]+)\b/);
+  return (matches && matches.length > 0)? matches[1] : null;
 }
