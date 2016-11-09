3fd047a2020cbc21bb2f9a77744dc4d9675fa9de
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2016-11-09 23:38:50 +0200

    Adding logic to render active tab and active body.

--- app/ui/components/RequestDetails.jsx
+++ app/ui/components/RequestDetails.jsx
@@ -8,64 +8,79 @@ export default class RequestDetails extends Component {
     const {
       itemState,
       request,
-      actions : { fetchRequestBody }
+      actions : { fetchRequestBody, fetchResponseBody }
     } = this.props;
 
-    if(!_.get(itemState, 'req.hasNoBody', false) && !_.get(itemState, 'req.bodyFetched', false)) {
-      fetchRequestBody(request);
+    const activeTab = _.get(itemState, 'activeTab', 'request');
+
+    if(activeTab === 'request') {
+      if(!_.get(itemState, 'req.hasNoBody', false) && !_.get(itemState, 'req.bodyFetched', false)) {
+        fetchRequestBody(request);
+      }
+    } else {
+      if(!_.get(itemState, 'res.bodyFetched', false)) {
+        fetchResponseBody(request);
+      }
     }
   }
 
-  renderRequestBody() {
+  renderHttpBody(stateName) {
     const {
       itemState
     } = this.props;
-    if(!_.has(itemState, 'req')) return false;
-    if(_.get(itemState, 'req.hasNoBody', true)) return false;
-    const reqState = itemState.req;
+    if(!_.has(itemState, stateName)) return false;
+    if(_.get(itemState, `${stateName}.hasNoBody`, true)) return false;
+    const state = itemState[stateName];
     return (
       <div>
         <h4 className='card-title'>Body</h4>
         <div className='card card-outline-secondary'>
-          <HttpBody state={reqState} />
+          <HttpBody state={state} />
         </div>
       </div>
     )
   }
 
+  renderNavTab(href, name, isActive) {
+    return (
+      <li className="nav-item">
+        <a className={isActive ? 'nav-link active' : 'nav-link'} data-toggle="tab" href={'#' + href} role="tab">{name}</a>
+      </li>
+    )
+  }
+
   render() {
     const {
+      itemState: {
+        activeTab: activeTab = 'request'
+      },
       request: {
         request,
-        // response
+        response
       }
     } = this.props;
     return (
       <div className='card'>
         <div className="card-header">
           <ul className="nav nav-tabs card-header-tabs float-xs-left" role="tablist">
-            <li className="nav-item">
-              <a className="nav-link active" data-toggle="tab" href="#request-details" role="tab">Request</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" data-toggle="tab" href="#response-details" role="tab">Respnse</a>
-            </li>
+            {this.renderNavTab('request', 'Request', activeTab === 'request')}
+            {this.renderNavTab('response', 'Response', activeTab === 'response')}
           </ul>
         </div>
         <div className="card-block">
-          <div id="request-details" role="tabpanel">
-            <h4 className='card-title'>Headers</h4>
-            <HttpHeaders headers={request.headers} />
-            { this.renderRequestBody() }
-          </div>
-          {
-            //   <div role="tabpanel">
-            //       <i>Headers</i>
-            //       { response ? (
-            //           <HttpHeaders headers={response.headers} />
-            //       ) : (<span>Not Available</span>) }
-            //   </div>
-          }
+          { activeTab === 'request' ? (
+            <div id="request" role="tabpanel">
+              <h4 className='card-title'>Headers</h4>
+              <HttpHeaders headers={request.headers} />
+              { this.renderHttpBody('req') }
+            </div>
+          ) : (
+            <div id="response" role="tabpanel">
+              <h4 className='card-title'>Headers</h4>
+              <HttpHeaders headers={response.headers} />
+              { this.renderHttpBody('res') }
+            </div>
+          )}
         </div>
       </div>
     )
--- test/ui/components/RequestDetails-spec.js
+++ test/ui/components/RequestDetails-spec.js
@@ -13,6 +13,7 @@ describe('components RequestDetails', () => {
       itemState: { },
       actions: {
         fetchRequestBody: sinon.spy(),
+        fetchResponseBody: sinon.spy(),
       },
     }, p);
 
@@ -24,7 +25,49 @@ describe('components RequestDetails', () => {
     };
   }
 
-  describe('req body', () => {
+  it('should render request as active tab by default', () => {
+    const { enzymeWrapper } = setup();
+    const requestTab = enzymeWrapper.find('[href="#request"]');
+    expect(requestTab.props().className).to.eql('nav-link active');
+
+    const responseTab = enzymeWrapper.find('[href="#response"]');
+    expect(responseTab.props().className).to.eql('nav-link');
+  });
+
+  it('should render request as active tab if set in state', () => {
+    const { enzymeWrapper } = setup({ itemState: { activeTab: 'request' } });
+    const requestTab = enzymeWrapper.find('[href="#request"]');
+    expect(requestTab.props().className).to.eql('nav-link active');
+
+    const responseTab = enzymeWrapper.find('[href="#response"]');
+    expect(responseTab.props().className).to.eql('nav-link');
+  });
+
+  it('should render response as active tab if set in state', () => {
+    const { enzymeWrapper } = setup({ itemState: { activeTab: 'response' } });
+    const requestTab = enzymeWrapper.find('[href="#request"]');
+    expect(requestTab.props().className).to.eql('nav-link');
+
+    const responseTab = enzymeWrapper.find('[href="#response"]');
+    expect(responseTab.props().className).to.eql('nav-link active');
+  });
+
+  describe('req', () => {
+    it('should be rendered by default', () => {
+      const { enzymeWrapper } = setup();
+      expect(enzymeWrapper.find('[id="request"]').length).to.eql(1);
+    });
+
+    it('should be rendered if activeTab is request', () => {
+      const { enzymeWrapper } = setup({ itemState: { activeTab: 'request' } });
+      expect(enzymeWrapper.find('[id="request"]').length).to.eql(1);
+    });
+
+    it('should not be rendered if activeTab is response', () => {
+      const { enzymeWrapper } = setup({ itemState: { activeTab: 'response' } });
+      expect(enzymeWrapper.find('[id="request"]').length).to.eql(0);
+    });
+
     it('should fetch request body', () => {
       const { props } = setup();
       expect(props.actions.fetchRequestBody).to.have.callCount(1);
@@ -43,22 +86,59 @@ describe('components RequestDetails', () => {
 
     it('should not render request body if no request state', () => {
       const { enzymeWrapper } = setup();
-      expect(enzymeWrapper.find('HttpBody').length).to.eql(0);
-      expect(enzymeWrapper.find('h4.card-title').length).to.eql(1);
+      expect(enzymeWrapper.find('[id="request"] HttpBody').length).to.eql(0);
+      expect(enzymeWrapper.find('[id="request"] h4.card-title').length).to.eql(1);
     });
 
     it('should not render request body if has no body', () => {
       const { enzymeWrapper } = setup({ itemState: { req: { hasNoBody: true } } });
-      expect(enzymeWrapper.find('HttpBody').length).to.eql(0);
-      expect(enzymeWrapper.find('h4.card-title').length).to.eql(1);
+      expect(enzymeWrapper.find('[id="request"] HttpBody').length).to.eql(0);
+      expect(enzymeWrapper.find('[id="request"] h4.card-title').length).to.eql(1);
     });
 
     it('should render request body if has body', () => {
       const reqState = { hasNoBody: false, dummy: new Date() };
       const { enzymeWrapper } = setup({ itemState: { req: reqState } });
-      const httpBody = enzymeWrapper.find('HttpBody');
+      const httpBody = enzymeWrapper.find('[id="request"] HttpBody');
       expect(httpBody.length).to.eql(1);
       expect(httpBody.props()).to.eql({ state: reqState });
+    });
+  });
+
+  describe('res', () => {
+    it('should be rendered if activeTab is response', () => {
+      const { enzymeWrapper } = setup({ itemState: { activeTab: 'response' } });
+      expect(enzymeWrapper.find('[id="response"]').length).to.eql(1);
+    });
+
+    it('should not be rendered if activeTab is request', () => {
+      const { enzymeWrapper } = setup({ itemState: { activeTab: 'request' } });
+      expect(enzymeWrapper.find('[id="response"]').length).to.eql(0);
+    });
+
+    it('should fetch response body', () => {
+      const { props } = setup({ itemState: { activeTab: 'response' } });
+      expect(props.actions.fetchResponseBody).to.have.callCount(1);
+      expect(props.actions.fetchResponseBody).to.have.been.calledWith(props.request);
+    });
+
+    it('should not fetch response body if already fetched', () => {
+      const { props } = setup({ itemState: { res: { bodyFetched: true } } });
+      expect(props.actions.fetchResponseBody).to.have.callCount(0);
+    });
+
+    it('should not render response body if no response state', () => {
+      const { enzymeWrapper } = setup();
+      expect(enzymeWrapper.find('HttpBody').length).to.eql(0);
+      expect(enzymeWrapper.find('h4.card-title').length).to.eql(1);
+    });
+
+    it('should render response body', () => {
+      const resState = { hasNoBody: false, dummy: new Date() };
+      const { enzymeWrapper } = setup({ itemState: { activeTab: 'response', res: resState } });
+      const httpBody = enzymeWrapper.find('[id="response"] HttpBody');
+      expect(httpBody.length).to.eql(1);
+      expect(httpBody.props()).to.eql({ state: resState });
       expect(enzymeWrapper.find('h4.card-title').length).to.eql(2);
     });
   });
